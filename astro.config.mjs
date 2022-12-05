8908814e42468d3821c14bcb1fe35e66d1b88857
Gustavo Dantas Galote <gusgalot3g@gmail.com>
2022-12-05 18:23:19 -0300

    refactor: replace md with mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 
-const isDev = process.env.NODE_ENV === "development";
-const test = isDev ? undefined : "/garden";
-
 // https://astro.build/config
 export default defineConfig({
-  site: isDev ? "https://ist4.github.com" : "https://ist4.github.com/garden",
-  base: isDev ? undefined : "/garden",
-  publicDir: "./public",
+  site: "https://ist4.github.com",
+  // base: "/garden",
   integrations: [
     tailwind({
       config: {
@@ -21,21 +18,8 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    mdx({
+      drafts: true,
+    }),
   ],
-  markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
-    shikiConfig: {
-      theme: "dracula",
-      wrap: true,
-    },
-    extendDefaultPlugins: true,
-  },
 });
